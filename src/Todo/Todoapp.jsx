ec30ccf6ab224e2f9d2fadd32fa82746ae70454b
shivam69x <159170281+shivam69x@users.noreply.github.com>
2025-04-18 02:29:14 +0530

    Updated functionality / UI changes / bug fixes

--- src/Todo/Todoapp.jsx
+++ src/Todo/Todoapp.jsx
@@ -13,10 +13,12 @@ function Todoapp() {
   const [completeTask, setCompleteTask] = useState(0);
   const [PendingTask, setPendingTask] = useState(0);
 
-  // ---{Dark to light mode}--------------code--
+  // ---{Dark to light mode}----------main problem in this is here this show erroe while runing---.
 
-  // const darkMode = useRef("");
-  // darkMode.current.style.backgroundColor = "red";
+  const darkMode = useRef("");
+  darkMode.current.style.backgroundColor = "red";
+
+  // ------this lower code is rining---.
 
   // const DarkMode = useRef(null);
 
